ad8a19a22cca8b483c2b9121ac134669aee59a79
Alicja Pasztelan <alicja.pasztelan@webchili.onmicrosoft.com>
2021-04-20 15:26:45 +0200

    Menu partly working

--- App_Part1/app/src/main/java/com/example/project/Menu.java
+++ App_Part1/app/src/main/java/com/example/project/Menu.java
@@ -1,8 +1,14 @@
 package com.example.project;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.widget.ConstraintLayout;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import com.example.project.grades.MA_Grades;
 
 public class Menu extends AppCompatActivity {
 
@@ -10,5 +16,27 @@ public class Menu extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_menu);
+
+        //ConstraintLayout cs_layout = (ConstraintLayout) findViewById(R.id.menu_layout);
+
+        Button firstapp = (Button) findViewById(R.id.firstapp_btn);
+        Button secondapp = (Button) findViewById(R.id.secapp_btn);
+
+        firstapp.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(Menu.this, MA_Grades.class));
+            }
+        });
+
+        secondapp.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
+
+
     }
 }
